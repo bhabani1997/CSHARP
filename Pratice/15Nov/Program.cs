4ddaa935adca50c0734a5f8b9aec08d7e44d8eb6
bhabani1997 <44669547+bhabani1997@users.noreply.github.com>
2018-11-17 20:03:08 +0530

    updated

--- Pratice/15Nov/Program.cs
+++ Pratice/15Nov/Program.cs
@@ -5,87 +5,87 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-//namespace _15Nov
-//{
-//    public abstract class details
-//    {
-//        string _gender;
-//        public int _age;
-//        public string name;
-//        public int id;
-//        public int score = 0;
-//        public string _firstname;
-//        string _lastname;
+namespace _15Nov
+{
+    public abstract class details
+    {
+        string _gender;
+        public int _age;
+        public string name;
+        public int id;
+        public int score = 0;
+        public string _firstname;
+        string _lastname;
 
-//        public abstract void Print_details();
-//        public abstract string Gender { get; }
+        public abstract void Print_details();
+        public abstract string Gender { get; }
 
-//    }
-//    public class players : details
-//        {
+    }
+    public class players : details
+    {
 
-//        public override void Print_details()
-//        {
-//            Console.WriteLine("Enter the name of the player");
-//            name = Console.ReadLine();
-//            Console.WriteLine("Enter the id of the player");
-//            id = Convert.ToInt32(Console.ReadLine());
-//            Console.WriteLine("Enter the score of the player");
-//            score = Convert.ToInt32(Console.ReadLine());
-//        }
+        public override void Print_details()
+        {
+            Console.WriteLine("Enter the name of the player");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter the id of the player");
+            id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the score of the player");
+            score = Convert.ToInt32(Console.ReadLine());
+        }
 
-//            public int Age
-//        {
-//            get
-//            {
-//                return _age;
-//            }
-//            set
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
 
-//            {
-//                _age = value;
-//            }
-//        }
-//        public override string Gender { get { return Gender; } }
-    
-//    }
-//    public class team : details
-//        {
-//            players[] p = new players[3];
-//            public override void Print_details()
-//            {
-//                Console.WriteLine("Enter the name of the team");
-//                name = Console.ReadLine();
-//                Console.WriteLine("Enter the id of the team");
-//                id = Convert.ToInt32(Console.ReadLine());
-//                Console.WriteLine("Enter the score of the team");
-//                score = Convert.ToInt32(Console.ReadLine());
-//                Console.WriteLine("Enter the score of the team");
+            {
+                _age = value;
+            }
+        }
+        public override string Gender { get { return Gender; } }
 
-//                for(int i=0;i<3;i++)
-//                {
-//                    p[i].Print_details();
-//                } 
+    }
+    public class team : details
+    {
+        players[] p = new players[3];
+        public override void Print_details()
+        {
+            Console.WriteLine("Enter the name of the team");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter the id of the team");
+            id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the score of the team");
+            score = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the score of the team");
 
-//            }
-//        public override string Gender { get { return Gender; } }
-//    }
+            for (int i = 0; i < 3; i++)
+            {
+                p[i].Print_details();
+            }
 
-  //class Program
-  //{
-  //   static void Main(string[] args)
-     // {
-          
-//            t1.Print_details();
-//            players p = new players();
-//            p._age = 31;
-//            Console.WriteLine(p.Age);
+        }
+        public override string Gender { get { return Gender; } }
+    }
 
-//            Console.ReadLine();
+    class Program
+    {
+        static void Main(string[] args)
+        {
 
-//        }
-  // }
-//}
+            t1.Print_details();
+            players p = new players();
+            p._age = 31;
+            Console.WriteLine(p.Age);
+
+            Console.ReadLine();
+
+        }
+    }
+}
 
 public interface ITransactions
 {
---
+++ Pratice/17Nov/Exception1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception
+{
+
+    //[Serializable]
+    //public class MyException : System.Exception
+    //{
+    //    public MyException() { }
+    //    public MyException(string message) : base(message) { }
+    //    public MyException(string message, System.Exception inner) : base(message, inner) { }
+    //    protected MyException(
+    //      System.Runtime.Serialization.SerializationInfo info,
+    //      System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    //}
+    //class practice
+    //{
+    //    public void handle_exception()
+    //    {
+    //        try
+    //        {
+    //            int x = 10;
+    //            int y = 0;
+    //            int z = x / y;
+    //        }
+
+    //        catch (ArithmeticException e)
+    //        {
+    //            Console.WriteLine("Arithmetic Exception occurs");
+    //        }
+    //        finally
+    //        {
+    //            Console.WriteLine("Messi is the greatest footballer of all time");
+    //        }
+    //    }
+    //}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //practice p1 = new practice();
+            //p1.handle_exception();
+            Console.ReadLine();
+        }
+    }
+
+
+    class MyException : ApplicationException
+    {
+        public void MyExceptiona()
+        {
+            Console.WriteLine("An exception occured");
+        }
+        public void MyDivideException()
+        {
+            Console.WriteLine("Exception occured, divisor should not be zero");
+        }
+    }
+
+    class TestMyException
+    {
+        public static void Main(String[] arg)
+        {
+            int d, div, res;
+            div = Int32.Parse(Console.ReadLine());
+            d = Int32.Parse(Console.ReadLine());
+            try
+            {
+                if (div == 0)
+                {
+                    throw new MyException();
+                }
+            }
+            catch (MyException e)
+            {
+                e.MyDivideException();
+            }
+
+            res = d / div;
+            Console.WriteLine("Result" + res);
+        }
+    }
+}
